313f637c06c77acbe2d3a4adc1aed528bc0afcdd
the-lyly <lyndadelaim@hotmail.com>
2024-04-22 16:49:13 +0100

    test

--- app.py
+++ app.py
@@ -1,26 +1,11 @@
 import streamlit as st
-import joblib
 
-# Load the trained logistic regression model
-knn = joblib.load('MRknn_model.joblib')
+# Title of the app
+st.title('My Streamlit App')
 
-# Streamlit webpage title
-st.title('Fraud Detection Model Deployment')
+# Asking the user for input
+user_input = st.text_input("Enter some text")
 
-# Input features into the model
-def user_input_features():
-    feature_1 = st.number_input('wilaya')
-    feature_2 = st.number_input('BP')
-    # Add as many features as you need
-    # ...
-    return [feature_1, feature_2]
-
-st.subheader('User Input Features')
-input_data = user_input_features()
-
-# When 'Predict' button is clicked, make a prediction and display it
-if st.button('Predict'):
-    prediction = log_reg.predict([input_data])
-    st.write(f'Prediction: {prediction[0]}')
-
-# Add additional instructions and information as needed
+# Displaying the input back to the user
+if user_input:
+    st.write(f"You entered: {user_input}")
